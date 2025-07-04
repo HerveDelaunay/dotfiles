238799cf35f87c8ba5ce0acbf2b2b5d6c59220e3
HerveDelaunay <delaunay.herve06@gmail.com>
2025-07-04 10:26:05 +0000

    add options.lua

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- disable scrolling animation
+vim.g.snacks_animate = false
+-- disable lazy import order warning
+vim.g.lazyvim_check_order = false
+-- disable case sensitivness while searching
+vim.opt.ignorecase = true
+-- disable line numbers
+vim.opt.number = false
+-- disable relative line numbers
+vim.opt.relativenumber = false
+-- keep the cursor in the middle while scrolling
+vim.opt.scrolloff = 8
