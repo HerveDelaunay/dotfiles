6a2e912d32702a26c629ab63398854d6e99d850c
HerveDelaunay <delaunay.herve06@gmail.com>
2025-07-15 12:20:22 +0000

    gruvbox-material colorscheme update

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,14 @@
 return {
 	-- add gruvbox
-	{ "wittyjudge/gruvbox-material.nvim" },
+	-- { "wittyjudge/gruvbox-material.nvim" },
+	{
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		opts = { contrast = "medium" },
+	},
+	{ "ellisonleao/gruvbox.nvim" },
 	-- Configure LazyVim to load gruvbox
 	{
 		"LazyVim/LazyVim",
